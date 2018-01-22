92b552887d96d3ff7525d242578728a04635c6b0
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-01-22 12:10:33 +0000

    commited end of task 1

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -4,7 +4,13 @@ import scala.io.StdIn
 
 object GreeterApplication extends App {
 
-  def greet(name : String) : Unit = println("Hello " + name)
+  def greet(name : String) : Unit = {
+    if (name == "adam") {
+      println("You don't get a hello!")
+    } else {
+      println("Hello")
+    }
+  }
 
   val name = StdIn.readLine("What is your name? ")
 
