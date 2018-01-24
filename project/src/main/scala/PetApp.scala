2611b4b17f0bf7296758f0485b2d017b09a5089d
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-01-24 14:23:52 +0000

    new abstract class, inheritance, polymorphsis, private properties, public methods, constructor arguments, conditional expression.

---
+++ project/src/main/scala/PetApp.scala
@@ -0,0 +1,87 @@
+object PetApp extends App {
+
+
+    abstract class Animal(name : String) {
+      def speak: String
+    }
+
+
+    class Cat (name : String) extends Animal(name) {
+      override def speak: String = "meow"
+
+    }
+
+    class Dog (name : String) extends Animal(name) {
+      override def speak: String = "woof"
+
+    }
+
+    class BorderTerrier (name : String) extends Dog(name) {
+
+      def hunt = "I am hunting rats"
+
+    }
+
+    class Tiger (name: String) extends Cat(name) {
+
+      def chase = "I am coming to get you"
+    }
+
+    class Wolf (name: String) extends Dog(name) {
+      override def speak: String = "howl"
+    }
+
+    class Meowth (name : String, coins : Int, rocket : Boolean) extends Cat(name) {
+
+      override def speak: String = s"I'm $name, I have $coins $single, " + rocketString
+
+      def rocketString: String = if (coins >= 10) "You are in team rocket" else "I am not worthy enough of #ROCKET"
+
+      private val single = if(coins < 2) "coin" else "coins"
+
+    }
+    class Puma (name : String, speed : Int, spots : Boolean, amount : Int) extends Cat(name) {
+
+      private val spotString = if (spots) "I have some spots" else "I have lots spots"
+
+      def amountString = if (amount >= 100) "You  have 100 or more spots, you are a male Puma"
+      else "You have less than 100 spots, you are a female Puma"
+
+
+      override def speak: String = s"I'm $name, I travel at $speed mph, $spotString, $amountString"
+    }
+
+    class Lion (name: String, speed : Int, gender : Boolean) extends Cat(name) {
+
+      private val genderString = if (gender) "I am Male" else "I am female"
+
+      override def speak: String = s"I'm $name, I travel at $speed mph & $genderString"
+    }
+
+
+    val jess : Animal = new Cat("jess")
+    val lassie : Animal = new Dog("lassie")
+    val sirius : Animal = new Wolf("sirius")
+    val meowth : Animal = new Meowth("meowth", 1, true)
+    val michael : Animal = new Puma("michael", 50, true, 70)
+    val connor : Animal = new Puma("connor", 70, false, 70)
+    val adam : Animal = new Dog("adam")
+    val terry : BorderTerrier = new BorderTerrier("terry")
+    val hugo : Tiger = new Tiger("hugo")
+    val matty : Animal = new Lion("matty", 35, true)
+
+
+    println(jess.speak)
+    println(lassie.speak)
+    println(sirius.speak)
+    println(meowth.speak)
+    println(michael.speak)
+    println(connor.speak)
+    println(adam.speak)
+    println(s"${terry.speak} ${terry.hunt}")
+    println(s"${hugo.speak} ${hugo.chase}")
+    println(matty.speak)
+}
+
+
+
--- project/src/main/scala/greetingApp2.scala
+++ project/src/main/scala/greetingApp2.scala
@@ -1,36 +1,102 @@
-package app
-
-import scala.io.StdIn
-
-
-class Person(name : String, age : Int) {
-
-  private val years : String = if(age == 1) "year" else "years"
-
-  def speak() : String = {
-
-    if (name == "doug") {
-
-      "You don't get a hello!"
-
-    }  else {
-
-      s"Hello $name, you are $age $years old."
-    }
-  }
-}
-
-object GreeterApplication extends App {
-
-object Prompt {
-
-  def ask(message : String) = StdIn.readLine(message)
-}
-
-  val name = Prompt.ask("What is your name?")
-  val age = Prompt.ask("How old are you?")
-  val person = new Person(name, age.toInt)
-
-  println(person.speak())
-
-}
+//package app
+//
+//import scala.io.StdIn
+//
+//abstract class BankAccount(accountNumber : String, val balance: Double) {
+//
+//  def withdraw(amount: Double)  : BankAccount
+//  def deposit(amount: Double) : BankAccount
+//
+//}
+//
+//class SavingsAccount(accountNumber: String, balance : Double) extends BankAccount(accountNumber, balance) {
+//
+//  override def deposit(amount: Double): SavingsAccount = new SavingsAccount(accountNumber, balance + amount)
+//  override def withdraw(amount: Double): SavingsAccount = new SavingsAccount(accountNumber, balance - amount)
+//
+//}
+//class cashISA(accountNumber: String, balance : Double) extends BankAccount(accountNumber, balance){
+//  override def deposit(amount: Double): cashISA = new cashISA(accountNumber, balance + amount)
+//  override def withdraw(amount: Double): cashISA = {
+//    println("Your account has low funds")
+//    this
+//  }
+//}
+//
+//class Person(name : String , age: Int) {
+//
+//  private val years : String = if(age == 1) "year" else "years"
+//
+//  def speak(): String = {
+//    if (name == "Jeff") {
+//      "You don't get a hello."
+//    } else {
+//      "Hello " + name + " " + "you are" + " " + age + " " + years + " old"
+//    }
+//  }
+//}
+//
+//object Prompt {
+//
+//  def ask(message : String) = StdIn.readLine(message)
+//
+//
+//}
+//
+//object GreeterApplication extends App {
+//
+//  val cashISA = new cashISA("12345" , 100)
+//  val cashISA100 = cashISA.withdraw(100)
+//
+//  val savingsAccount = new SavingsAccount("12345", 100.00)
+//  val savingsPlus100 = savingsAccount.deposit(50.00)
+//
+//  val name = Prompt.ask("What is your name? ")
+//  val age = Prompt.ask("What is your age? ")
+//
+//  val p : Person = new Person(name, age.toInt)
+//  println(p.speak())
+//  println(savingsAccount.balance)
+//
+//}
+//
+//
+//class Person(name : String, age : Int)
+//{
+//  private val years : String = if(age == 1) "year" else "years"
+//
+//  def speak() : String = {
+//
+//    if (name == "doug") {
+//
+//      "You don't get a hello!"
+//
+//    }  else {
+//
+//      s"Hello $name, you are $age $years old."
+//    }
+//  }
+//}
+//
+//
+//
+//
+//object GreeterApplication extends App {
+//
+//object Prompt {
+//
+//  def ask(message : String) = StdIn.readLine(message)
+//}
+//
+//  val name = Prompt.ask("What is your name?")
+//  val age = Prompt.ask("How old are you?")
+//  val person = new Person(name, age.toInt)
+//
+//  println(person.speak())
+//
+//}
+//
+//
+//
+//
+//
